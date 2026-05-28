b725398fe768fb4072e039958838b52777c80d2d
Артемий Васильев <artemiyfraxis@gmail.com>
2026-05-28 16:52:56 +0300

    Add clasp config and deploy script

---
+++ deploy.ps1
@@ -0,0 +1,3 @@
+$env:PATH += ";C:\Program Files\nodejs;C:\Users\Артемий Васильев\AppData\Roaming\npm"
+Set-Location $PSScriptRoot
+& "C:\Users\Артемий Васильев\AppData\Roaming\npm\clasp.cmd" push --force
